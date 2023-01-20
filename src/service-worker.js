9e3d3d2e874ea89189f4f13b148d1a5c64c562ab
blas-rodriguez <blasrodriguez934@gmail.com>
2023-01-20 15:16:44 -0300

    4

--- src/service-worker.js
+++ src/service-worker.js
@@ -44,7 +44,7 @@ registerRoute(
     return true;
   },
   // createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
-  createHandlerBoundToURL('https://mac-queen.com/nuevo15/')
+  createHandlerBoundToURL('https://mac-queen.com/nuevo15')
 );
 
 // An example runtime caching route for requests that aren't handled by the
